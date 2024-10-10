b446af4abe7aa124816d1a2eedac958bf84d1bba
Anton Shokuev <toddchavez@mail.ru>
2024-10-10 23:39:30 +0300

    homework4.10

---
+++ 410classwork.py
@@ -0,0 +1,6 @@
+stroka = input()
+i = 0
+for bukva in stroka:
+  if bukva == 'н':
+    i += 1
+print(i)
---
+++ python4.py
@@ -0,0 +1,18 @@
+# stroka = input()
+# dlina = 0
+# p = 1
+# stroka.replace('н', '!')
+# for i in range(len(stroka) - 1):
+#   if stroka[i] == stroka[i+1]:
+#     p += 1
+#     dlina = max(dlina, p)
+#   else:
+#     p = 1
+# print(dlina, stroka)
+
+# stroka = input()
+# short_stroka = stroka[stroka.find('(') + 1 : stroka.find(')')]
+# print(short_stroka)
+
+stroka = input()
+lst = stroka.split(" ")
