9642bf0e570802691b7d4cd7b6cc70577d97f064
Anton Shokuev <toddchavez@mail.ru>
2024-10-11 11:57:46 +0300

    homewotk4

--- python4.py
+++ python4.py
@@ -1,18 +1,21 @@
-# stroka = input()
-# dlina = 0
-# p = 1
-# stroka.replace('н', '!')
-# for i in range(len(stroka) - 1):
-#   if stroka[i] == stroka[i+1]:
-#     p += 1
-#     dlina = max(dlina, p)
-#   else:
-#     p = 1
-# print(dlina, stroka)
+stroka = input()
+dlina = 0
+p = 1
+stroka.replace('н', '!')
+for i in range(len(stroka) - 1):
+  if stroka[i] == stroka[i+1]:
+    p += 1
+    dlina = max(dlina, p)
+  else:
+    p = 1
+print(dlina, stroka)
 
-# stroka = input()
-# short_stroka = stroka[stroka.find('(') + 1 : stroka.find(')')]
-# print(short_stroka)
+stroka = input()
+short_stroka = stroka[stroka.find('(') + 1 : stroka.find(')')]
+print(short_stroka)
 
 stroka = input()
 lst = stroka.split(" ")
+for i in range(len(lst)):
+  if lst[i][0] == 'а' and lst[i][-1] == 'я':
+    print(lst[i])
