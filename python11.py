c852312a8c98cc8d7c1c76e66f9a3467284daf5a
SpiderJerusalemm <144051363+SpiderJerusalemm@users.noreply.github.com>
2024-12-13 03:37:51 +0700

    Update python11.py

--- python11.py
+++ python11.py
@@ -33,3 +33,16 @@ df['CBF_01'] = df['CBF_01'].replace(99, 'miss', regex=True)
 bm = df.groupby('CBF_01')['P_NUMFLU'].mean()
 bm_tuple = (round(float(bm[1]), 1), round(float(bm[2]), 1))
 print(bm_tuple)
+
+#3 задание
+df = pd.read_csv("NISPUF17.csv", sep=',')
+dfMale = df[df["SEX"] == 1]
+dfFemale = df[df["SEX"] == 2]
+
+maleCpx = dfMale[(dfMale["HAD_CPOX"] == 1) & (dfMale["P_NUMVRC"] > 0)].shape[0]/dfMale[(dfMale["HAD_CPOX"] == 2) & (dfMale["P_NUMVRC"] > 0)].shape[0]
+femaleCpx = dfFemale[(dfFemale["HAD_CPOX"] == 1) & (dfFemale["P_NUMVRC"] > 0)].shape[0]/dfFemale[(dfFemale["HAD_CPOX"] == 2) & (dfFemale["P_NUMVRC"] > 0)].shape[0]
+
+print(
+    {"male": round(maleCpx, 4),
+        "female": round(femaleCpx, 4)}
+)
