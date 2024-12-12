68d3d2c4cdc21c1ed898e9a442e5548e2110b290
SpiderJerusalemm <144051363+SpiderJerusalemm@users.noreply.github.com>
2024-12-12 21:53:39 +0700

    Update python6.py

--- python6.py
+++ python6.py
@@ -1,33 +1,40 @@
-# def m_stroka(row):
-#   sum = 0
-#   for element in row:
-#     sum += element
-#   return sum
-
-
-# arr = [[3, 1, 4], [1, 5 , 7], [7, 0, 0], [9,3,6]]
-# s = list()
-
-# for row in arr:
-#   sum_of_row = m_stroka(row)
-#   s.append(sum_of_row)
-
-# print(s,'\n',r'max is', max(s), '\n',r'min is', min(s))
-
-
-def chet_nechet(min_el):
+def m_stroka(row):
+  sum = 0
   for element in row:
-    if element % 2 == 0:
-      return(0)
-    else:
-      return(1)
+    sum += element
+  return sum
 
-arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+def min_max(row):
+  return min(row), max(row)
+
+arr = [[3, 1, 4], [1, 5 , 7], [7, 0, 0], [9,3,6]]
 s = list()
 
 for row in arr:
-  min_el = min(row)
-  sum_of_row = chet_nechet(min_el)
+  sum_of_row = m_stroka(row)
   s.append(sum_of_row)
 
-print(arr, s)
+print(s,'\n',r'max is', max(row), 'sum is', max(s), '\n',r'min is', min(row), 'sum is', min(s))
+
+
+
+def chet_nechet(min_el):
+    if min_el % 2 == 0:
+        return 0
+    else:
+        return 1
+
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+new_arr = []
+
+for row in arr:
+    min_el = min(row)
+    row_copy = row[:]
+    for i in range(len(row_copy)):
+        if row_copy[i] == min_el:
+            row_copy[i] = chet_nechet(min_el)
+            break
+    new_arr.append(row_copy)
+
+for row in new_arr:
+    print(row)
